a30e75800d069846f20a6859898ca299a7c45e2d
David Potter <dave@iapetus.i.dnhome.uk>
2020-06-13 13:26:43 +0100

    Build script - add minification after build using go minify

--- _build.sh
+++ _build.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
 
+# Can't find the build tool? Best do nothing
 if ! command -v cobalt > /dev/null; then
     exit 1
 fi
 
+# Clean
 if [ -d "_site" ]; then
+    echo "Clean..."
     rm -rf "_site"
 fi
 
+# Build
+echo "Build..."
 cobalt build
 
+# Link for rss feed to preserve old permalink
 if [ -f "_site/feed.xml" ]; then
     ln -s "feed.xml" "_site/rss.xml"
 fi
+
+# Minify ALL THE THINGS!
+if ! command -v minify > /dev/null; then
+    exit 1
+fi
+
+if [ ! -d "_site" ]; then
+    exit 1
+fi
+
+echo "Minify..."
+minify -o _site/ -r _site/
